9b5c21e7bf65875aeeccb2a3e8c9b5b61d2bd871
Pranab Ghosh <pranabit6@gmail.com>
2024-07-27 23:45:45 +0530

    file handling using python

---
+++ 5 - Modules and Packages/package/__init__.py
@@ -0,0 +1,2 @@
+# This is a special file that tells Python that this directory should be considered a package.
+# This file can be empty, but it can also execute initialization code for the package or set the __all__ variable.
